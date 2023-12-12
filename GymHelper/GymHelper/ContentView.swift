4d8daa18987a62cff48c1e9c47ea2b8d61efb42d
mubarizz <mubarizzahmadov@gmail.com>
2023-12-12 22:16:27 +0300

    False commit before

--- GymHelper/GymHelper/ContentView.swift
+++ GymHelper/GymHelper/ContentView.swift
@@ -7,18 +7,89 @@
 
 import SwiftUI
 
+
+struct exercise: Identifiable{
+    var id = UUID()
+    var name: String
+    var reps: String
+    var sets: String
+    var weight: String
+}
+
+
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var reps: String = ""
+    @State private var sets: String = ""
+    @State private var weight: String = ""
+    @ObservedObject var ExerciseData = ExerciseClass()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            NavigationView{
+
+                       Form{
+                           
+                           Section(header: Text("Exercise Information")) {
+                               TextField("Name", text: $name)
+                               TextField("Reps", text: $reps)
+                               TextField("Sets", text: $sets)
+                               TextField("Weight", text: $weight)
+
+                           }
+                           Section {
+                                       Button("Add Exercise") {
+                                           ExerciseData.addExercise(name: name, reps: reps, sets: sets, weight: weight)
+                                               name = ""
+                                               reps = ""
+                                               sets = ""
+                                               weight = ""
+                                               }
+                                           }
+                           Section(header: Text("Exercise List")) {
+                                               ForEach(ExerciseData.exercises) { exercise in
+                                                   Text("\(exercise.name), \(exercise.reps), \(exercise.sets), \(exercise.weight)")
+                                               }
+                                           }
+                           Button("Clear the List"){
+                               ExerciseData.clearExercises()
+                           }
+                           Button("Save the Data"){
+                               ExerciseData.saveExercises()
+                           }
+                           Button("Upload the Data"){
+                               ExerciseData.loadExercises()
+                           }
+                           
+                       }
+                       .background(Color(hex: "092635"))
+                       .navigationTitle("Add Exercise ")
+                   }
         }
-        .padding()
+        
+        
+
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+extension Color {
+    init(hex: String) {
+        var cleanHexCode = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        cleanHexCode = cleanHexCode.replacingOccurrences(of: "#", with: "")
+        print(cleanHexCode)
+        var rgb: UInt64 = 0
+        
+        Scanner(string: cleanHexCode).scanHexInt64(&rgb)
+        
+        let redValue = Double((rgb >> 16) & 0xFF) / 255.0
+        let greenValue = Double((rgb >> 8) & 0xFF) / 255.0
+        let blueValue = Double(rgb & 0xFF) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue)
+    }
 }
--- GymHelper/GymHelper/ExerciseModell.swift
+++ GymHelper/GymHelper/ExerciseModell.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class ExerciseClass: ObservableObject {
+    @Published var exercises: [Exercise] = []
+
+    init() {
+        loadExercises()
+    }
+
+    func addExercise(name: String, reps: String, sets: String, weight: String) {
+        let newExercise = Exercise(name: name, reps: reps, sets: sets, weight: weight)
+        exercises.append(newExercise)
+        saveExercises()
+    }
+
+    func clearExercises() {
+        exercises.removeAll()
+        saveExercises()
+    }
+
+    func saveExercises() {
+        if let encodedData = try? JSONEncoder().encode(exercises) {
+            UserDefaults.standard.set(encodedData, forKey: "exercises")
+        }
+    }
+
+
+    func loadExercises() {
+        if let encodedData = UserDefaults.standard.data(forKey: "exercises"),
+           let decodedExercises = try? JSONDecoder().decode([Exercise].self, from: encodedData) {
+               exercises = decodedExercises
+        }
+    }
+
+}
+
+struct Exercise: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var reps: String
+    var sets: String
+    var weight: String
+}
