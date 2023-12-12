a239fddec4f07f7599d14c57427dc2ce47a5b0c2
mubarizz <mubarizzahmadov@gmail.com>
2023-12-12 22:14:55 +0300

    Seperated Files, Available for iphone

---
+++ GymHelper/GymHelper/ExerciseModell.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseModell.swift
+//  GymHelper
+//
+//  Created by Mubarız Ahmadov on 12.12.2023.
+//
+
+import Foundation
---
+++ GymHelper/GymHelper/GymHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymHelperApp.swift
+//  GymHelper
+//
+//  Created by Mubarız Ahmadov on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GymHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
